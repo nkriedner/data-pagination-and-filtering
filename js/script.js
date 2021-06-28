0540c89137465fd733ffe3c1c18ee1298857109f
Nils Kriedner <nkriedner@googlemail.com>
2021-06-28 16:17:14 +0200

    Add functionality to search component + add pagination for search results + handle no search results

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
 function showPage(list, page) {
     const startIndex = page * 9 - 9;
     const endIndex = page * 9;
@@ -23,22 +8,18 @@ function showPage(list, page) {
             const li = document.createElement("li");
             li.classList.add("student-item", "cf");
             li.innerHTML = `<div class="student-details">
-                              <img class="avatar" src=${data[i].picture.large} alt="Profile Picture">
-                              <h3>${data[i].name.first} ${data[i].name.last}</h3>
-                              <span class="email">${data[i].email}</span>
+                              <img class="avatar" src=${list[i].picture.large} alt="Profile Picture">
+                              <h3>${list[i].name.first} ${list[i].name.last}</h3>
+                              <span class="email">${list[i].email}</span>
                            </div>
                            <div class="joined-details">
-                              <span class="date">Joined ${data[i].registered.date}</span>
+                              <span class="date">Joined ${list[i].registered.date}</span>
                            </div>`;
             ul.appendChild(li);
         }
     }
 }
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
 function addPagination(list) {
     let numberOfPages = Math.ceil(list.length / 9);
     const ul = document.querySelector(".link-list");
@@ -51,13 +32,21 @@ function addPagination(list) {
         li.appendChild(button);
         ul.appendChild(li);
     }
+    if (!ul.firstChild) {
+        console.log("NO RESULTS");
+        const noResultMessage = document.createElement("h2");
+        noResultMessage.style.fontSize = "2rem";
+        noResultMessage.textContent = "No Results for your search query";
+        ul.appendChild(noResultMessage);
+        return;
+    }
     ul.firstChild.firstChild.className = "active";
     ul.addEventListener("click", (e) => {
         if (e.target.tagName == "BUTTON") {
             const lis = ul.childNodes;
             // Remove active class from any other button
             for (let i = 0; i < lis.length; i++) {
-                console.log(lis[i]);
+                //  console.log(lis[i]);
                 if (lis[i].firstChild.className == "active") {
                     lis[i].firstChild.classList.remove("active");
                 }
@@ -66,7 +55,7 @@ function addPagination(list) {
             e.target.className = "active";
             // Call the showPage function
             const pageNumber = parseInt(e.target.textContent);
-            showPage(data, pageNumber);
+            showPage(newStudentList, pageNumber);
         }
     });
 }
@@ -77,12 +66,39 @@ addPagination(data);
 
 // Add a search component
 const searchBarLocation = document.querySelector(".header");
-// console.log(searchBarLocation);
 const label = document.createElement("label");
 label.setAttribute("for", "search");
 label.className = "student-search";
 label.innerHTML = `<span>Search by name</span>
                    <input id="search" placeholder="Search by name...">
                    <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>`;
-console.log(label);
 searchBarLocation.append(label);
+
+const searchInput = searchBarLocation.querySelector("input");
+const button = document.querySelector("button");
+
+searchInput.addEventListener("keyup", () => {
+    searchStudents();
+});
+
+button.addEventListener("click", () => {
+    console.log("CLICK");
+    searchStudents();
+});
+
+let newStudentList = data;
+function searchStudents() {
+    newStudentList = [];
+    let userInput = searchInput.value.toLowerCase();
+    // Loop through data:
+    for (let i = 0; i < data.length; i++) {
+        if (
+            data[i].name.first.toLowerCase().includes(userInput) ||
+            data[i].name.last.toLowerCase().includes(userInput)
+        ) {
+            newStudentList.push(data[i]);
+        }
+    }
+    showPage(newStudentList, 1);
+    addPagination(newStudentList);
+}
